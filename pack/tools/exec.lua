451583bce7b30421637db9672a44e66c39d98b87
mozers <mozers@localhost>
2007-09-28 07:26:25 +0000

    -chg: HSolo ??????? 2 ??????? <http://forum.ru-board.com/topic.cgi?forum=5&topic=3215&start=2020#3>

--- pack/tools/exec.lua
+++ pack/tools/exec.lua
@@ -1,8 +1,11 @@
+-- Exec.lua
+-- Version: 1.2
+-- Autor: HSolo, mozers™
+---------------------------------------------------
 -- Расчет выделенного текста как математического выражения
 -- или открытие в браузере выделенного URL
--- Автор: HSolo, mozers™
--- http://forum.ru-board.com/topic.cgi?forum=5&topic=3215&start=1120#19
---------------------------------------------
+-- http://forum.ru-board.com/topic.cgi?forum=5&topic=3215&start=2020#3
+---------------------------------------------------
 
 local function FormulaDetect(str)
   local PatternNum = "([\-\+\*\/%b()%s]*%d+[\.\,]*%d*[\)]*)"
@@ -14,21 +17,31 @@ local function FormulaDetect(str)
       if startPos == nil then break end
       startPos = endPos + 1
 --~ print(Num)
-      Num = string.gsub (Num, '%s+', '') -- Убираем пробелы
-      Num = string.gsub (Num, '^([\(%d]+)', '+%1') -- Там, где перед числом нет знака, ставим "+" (т.е. пробелы и переводы строк заменяются на "+")
-      Num = string.gsub (Num, '^([\)]+)([%d]+)', '%1+%2') -- Добавляем знак "+" (при его отсутствии) между числом и скобкой
-      Formula = Formula..Num -- Склеиваем вновь преобразованную строку
+      Num = string.gsub (Num, '%s+', '')                           -- Убираем пробелы
+      Num = string.gsub (Num, '^([\(%d]+)', '+%1')                 -- Там, где перед числом нет знака, ставим "+" (т.е. пробелы и переводы строк заменяются на "+")
+      Num = string.gsub (Num, '^([\)]+)([%d]+)', '%1+%2')          -- Добавляем знак "+" (при его отсутствии) между числом и скобкой
+      Formula = Formula..Num                                       -- Склеиваем вновь преобразованную строку
   end
-  Formula = string.gsub (Formula, '^[\+]', '')                    -- В самом начале получился лишний "+" - убиваем его
-  Formula = string.gsub(Formula,"[\,]+",'.')                      -- Не будем строги к символу - разделителю десятичных чисел :)
-  Formula = string.gsub(Formula,"([\+\*\/\-])([\+\*\/\-]+)",'%1') -- Удаляем сдвоенные знаки
-  Formula = string.gsub(Formula,"+",' + ')                        -- Разделяем суммы пробелами (имхо, нагляднее чем разделять все знаки)
+  Formula = string.gsub (Formula, '^[\+]', '')                     -- В самом начале получился лишний "+" - убиваем его
+  Formula = string.gsub(Formula,"[\,]+",'.')                       -- Не будем строги к символу - разделителю десятичных чисел :)
+  Formula = string.gsub(Formula,"([\+])([\+]+)",'%1')              -- Удаляем сдвоенные знаки (++) = (+)
+  Formula = string.gsub(Formula,"([\-])([\+]+)",'%1')              -- Удаляем сдвоенные знаки (-+) = (-)
+
+  Formula = string.gsub(Formula,"([\+\-\*\/])([\*\/]+)",'%1')      -- Удаляем сдвоенные знаки перед * и / т.к. это явный косяк
+  Formula = string.gsub(Formula,"([\+\-\*\/])([\*\/]+)",'%1')      -- Для успокоения совести проделаем дважды
+
+  Formula = string.gsub(Formula,"([%d\)]+)([\+\*\/\-])",'%1 %2 ')  -- Разделяем группы пробелами
+
   return Formula
 end
 
--- local str = props['CurrentSelection'] -- неверно работает с вертикальными блоками
--- Кстати - явный BUG ядра - может кто исправит?
-local str = editor:GetSelText()
+local str = ''
+if editor.Focus then
+  str = editor:GetSelText()
+else
+  str = props['CurrentSelection']
+end
+
 if (str == '') then
   str = editor:GetCurLine()
 end
@@ -51,7 +64,7 @@ if (string.len(str) > 2) then
     editor:LineEnd()
     local sel_start = editor.SelectionStart + 1
     local sel_end = sel_start + string.len(res)
-    editor:AddText('\n'..res)
+    editor:AddText('\n= '..res)
     editor:SetSel(sel_start, sel_end)
     print('>> Результат: '..res)
   end
@@ -63,8 +76,6 @@ end
 
 --~ dmfdmk v15*6dmd.ks skm4.37/3d(k)gm/sk+d skdmg(6,7+6)skdmgk
 
---~ Колбаса = 24.5кг. * 120руб./кг 
---~ Бензин(ABC) = (2500км. / (11,5л./100км.)) * 18.4руб./л + Канистра =100руб. 
+--~ Колбаса = 24.5кг. * 120руб./кг
+--~ Бензин(ABC) = (2500км. / (11,5л./100км.)) * 18.4руб./л + Канистра =100руб.
 --~ Штукатурка = 22.4 м2 /80руб./100 м2
-
---~ http://forum.ru-board.com/topic.cgi?forum=2&topic=3339&start=160#lt
--- pack/tools/translit.lua
+++ pack/tools/translit.lua
@@ -1,46 +1,43 @@
 -- Модуль translit.lua предназначен для транслитерации английских букв в русские
--- Version: 1.01
+-- Version: 1.02
 -- Autor: HSolo
 -- Подключение можно сделать похожим образом:
 ---------------------------------------------------------------------------------
 --~ command.name.84.*=Translitiration
 --~ command.84.*=dofile $(SciteDefaultHome)\tools\translit.lua
 --~ command.mode.84.*=subsystem:lua,savebefore:no
---~ command.shortcut.84.*=Alt+T
+--~ command.shortcut.84.*=Alt+Q
 ---------------------------------------------------
-local translit = {['shh']="щ",
-  ['jo']="ё", ['yo']="ё", ['zh']="ж", ['ii']="й", ['jj']="й", ['sh']="ш",
-  ['ch']="ч", ['je']="э", ['ju']="ю", ['yu']="ю", ['ja']="я", ['ya']="я",
-  ['a'] ="а", ['b'] ="б", ['v'] ="в", ['w'] ="в", ['g'] ="г",
-  ['d'] ="д", ['e'] ="е", ['z'] ="з", ['i'] ="и", ['j'] ="й",
-  ['k'] ="к", ['l'] ="л", ['m'] ="м", ['n'] ="н", ['o'] ="о", ['~']="ъ",
-  ['p'] ="п", ['r'] ="р", ['s'] ="с", ['t'] ="т", ['u'] ="у", ['\"']="ъ",
-  ['f'] ="ф", ['h'] ="х", ['x'] ="х", ['c'] ="ц", ['y'] ="ы", ['\'']="ь",
-  ['Shh']="Щ",
-  ['Jo']="Ё", ['Yo']="Ё", ['Zh']="Ж", ['Ii']="Й", ['Jj']="Й", ['Sh']="Ш",
-  ['Ch']="Ч", ['Je']="Э", ['Ju']="Ю", ['Yu']="Ю", ['Ja']="Я", ['Ya']="Я",
-  ['A'] ="А", ['B'] ="Б", ['V'] ="В", ['W'] ="В", ['G'] ="Г",
-  ['D'] ="Д", ['E'] ="Е", ['Z'] ="З", ['I'] ="И", ['J'] ="Й",
-  ['K'] ="К", ['L'] ="Л", ['M'] ="М", ['N'] ="Н", ['O'] ="О",
-  ['P'] ="П", ['R'] ="Р", ['S'] ="С", ['T'] ="Т", ['U'] ="У",
-  ['F'] ="Ф", ['H'] ="Х", ['X'] ="Х", ['C'] ="Ц", ['Y'] ="Ы"
-  }
+
+local translit = {
+  ['a'] ="а", ['b'] ="б", ['v'] ="в", ['w'] ="в", ['g'] ="г", ['d'] ="д", ['e'] ="е", ['jo']="ё",
+  ['yo']="ё", ['zh']="ж", ['z'] ="з", ['i'] ="и", ['j'] ="й", ['jj']="й", ['k'] ="к", ['l'] ="л",
+  ['m'] ="м", ['n'] ="н", ['o'] ="о", ['p'] ="п", ['r'] ="р", ['s'] ="с", ['t'] ="т", ['u'] ="у",
+  ['f'] ="ф", ['h'] ="х", ['x'] ="х", ['c'] ="ц", ['ch']="ч", ['sh']="ш", ['shh']="щ",['\'']="ь",
+  ['y'] ="ы", ['\"']="ъ", ['~']="ъ",  ['je']="э", ['ju']="ю", ['yu']="ю", ['ja']="я", ['ya']="я"}
+
+local function RusUpper(Ch)
+  if string.byte(Ch) < 224 then return string.upper(Ch) end
+  return string.char(string.byte(Ch)-32)
+end
 
 local function TranslitIT(s)
+  if string.len(s) == 0 then return outstr end
+
   local pos = 1
   local outstr = ""
   local res
   local toFind
 
-  if string.len(s) == 0 then
-    return outstr
-  end
-
   while (pos <= string.len(s)) do
+    local isCapital = true
+    if string.sub(s, pos, pos) == string.lower(string.sub(s, pos, pos)) then isCapital = false end
+
     for i = 3, 1, -1 do
-      toFind = string.sub(s, pos, pos + i - 1)
+      toFind = string.lower(string.sub(s, pos, pos + i - 1))
       res = translit[toFind]
       if res ~= nil then
+        if isCapital then res = RusUpper(res) end
         outstr = outstr..res
         pos = pos + string.len(toFind)
         break
@@ -54,19 +51,17 @@ local function TranslitIT(s)
   return outstr
 end
 
-local str = props['CurrentSelection']
-if (str == '') then
-    str = editor:GetSelText()
+local str = ''
+if editor.Focus then
+  str = editor:GetSelText()
+else
+  str = props['CurrentSelection']
 end
 if (str == '') then
   str = editor:GetCurLine()
+  editor:LineDelete()
 end
 
 local result = TranslitIT(str)
-editor:CharRight()
-editor:LineEnd()
-local sel_start = editor.SelectionStart + 1
-local sel_end = sel_start + string.len(result)
-editor:AddText('\n'..result)
-editor:SetSel(sel_start, sel_end)
+editor:ReplaceSel(result)
 print(result)
