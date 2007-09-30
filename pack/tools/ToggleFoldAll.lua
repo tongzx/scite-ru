f01aca1bb464a28187ba95b301c3d5d55a2e1c5c
mozers <mozers@localhost>
2007-09-30 11:00:41 +0000

    -chg: ??????? ?? ???????? ??? ???????? ?????????????? ???????? ?????? ?? ????????? ??????, ???????????? ??? ??????? SciTE (??? ?? ??????????? ?? ? ???? ?????? ?? ????) ?? ???????? ???????? ?????? (??? ???? ??????????? ???? ???? ????? ??????????, ???????? ? fold.on.open.ext).

--- pack/tools/ToggleFoldAll.lua
+++ pack/tools/ToggleFoldAll.lua
@@ -1,5 +1,5 @@
 -- ToggleFoldAll.lua
--- Version: 1.2
+-- Version: 1.3
 -- Autor: mozers™
 ---------------------------------------------------
 -- Скрипт для автоматического сворачивания всех секций при открытии файлов заданного типа
@@ -10,18 +10,11 @@
 --     fold.on.open.ext=properties,ini
 ---------------------------------------------------
 
-local bToggleFoldAll=false
-
-local function CheckFoldExpanded()
-	for i = 1, editor.LineCount-1 do
-		if not editor.FoldExpanded[i] then return true end
-	end
-	return false
-end
+local IsSciteStarted = false
 
 local function CheckExt()
 	local toggle_foldall_ext = string.upper(props['fold.on.open.ext'])
-	local file_ext = '('..string.upper(props['FileExt'])..')'
+	local file_ext = string.upper(props['FileExt'])
 	if string.find(toggle_foldall_ext, file_ext) ~= nil then
 		return true
 	end
@@ -29,10 +22,9 @@ local function CheckExt()
 end
 
 local function ToggleFoldAll()
-	if not CheckFoldExpanded() then
+	if IsSciteStarted and CheckExt() then
 		scite.MenuCommand ("IDM_TOGGLE_FOLDALL")
 	end
-	bToggleFoldAll=false
 end
 
 -- Add user event handler OnOpen
@@ -40,11 +32,7 @@ local old_OnOpen = OnOpen
 function OnOpen(file)
 	local result
 	if old_OnOpen then result = old_OnOpen(file) end
-	if CheckExt() then
-		bToggleFoldAll=true
-	else
-		bToggleFoldAll=false
-	end
+	if ToggleFoldAll() then return true end
 	return result
 end
 
@@ -53,8 +41,6 @@ local old_OnUpdateUI = OnUpdateUI
 function OnUpdateUI ()
 	local result
 	if old_OnUpdateUI then result = old_OnUpdateUI() end
-	if bToggleFoldAll then
-		if ToggleFoldAll() then return true end
-	end
+	IsSciteStarted = true
 	return result
 end
