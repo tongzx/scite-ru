0308f5f2ea8628edf99a94b1c2459855e4baa305
mozers <mozers@localhost>
2007-11-24 20:40:51 +0000

    -chg: ???? ????????? ????????? [import] (?????: mimir) ???????? ????????????? ????????? import ? ?????? *.abbrev

--- src/scite/src/PropSetFile.cxx
+++ src/scite/src/PropSetFile.cxx
@@ -93,6 +93,7 @@ bool PropSetFile::ReadLine(const char *lineBuffer, bool ifIsTrue, FilePath direc
 		ifIsTrue = GetInt(expr) != 0;
 	} else if (isprefix(lineBuffer, "import ") && directoryForImports.IsSet()) {
 		SString importName(lineBuffer + strlen("import") + 1);
+		if(!importName.contains('.')) //!-add-[import]
 		importName += ".properties";
 		FilePath importPath(directoryForImports, FilePath(importName.c_str()));
 		if (Read(importPath, directoryForImports, imports, sizeImports)) {
