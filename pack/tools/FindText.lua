4b2ce6a216399641dcf3cddaca9c58bee2ae3359
mozers <mozers@localhost>
2007-12-07 10:59:59 +0000

    -chg: FindText.lua ????????. ??????? ????????, ?????????? ? Issue 31

--- pack/tools/FindText.lua
+++ pack/tools/FindText.lua
@@ -1,4 +1,4 @@
--- FindText v5.1
+-- FindText v6.0
 -- Автор: неизвесен <http://forum.ruteam.ru/index.php?action=vthread&forum=22&topic=175>
 -- Корректировки: mozers™, mimir, Алексей
 -- Поиск выделенного в окне редактора (или консоли) текста с выводом содержащих его строк в консоль
@@ -9,19 +9,13 @@
 --    command.mode.22.*=subsystem:lua,savebefore:no
 -----------------------------------------------
 
-local function UnderlineText(start, length)
-	editor.IndicStyle[0] = INDIC_ROUNDBOX
-	editor.IndicStyle[1] = INDIC_ROUNDBOX
-	editor.IndicStyle[2] = INDIC_ROUNDBOX
-	editor.IndicFore[0] = 255*256*256 -- BLUE
-	editor.IndicFore[1] = 255         -- RED
-	editor.IndicFore[2] = 255*256     -- GREEN
-	editor:StartStyling(start,INDICS_MASK)
-	if length == -1 then
-		editor:SetStyling(editor.Length, 0)
-	else
-		editor:SetStyling(length,INDIC2_MASK)
-	end
+local function RemoveFindMarks()
+	scite.SendEditor(SCI_SETINDICATORCURRENT, INDIC_CONTAINER)
+	scite.SendEditor(SCI_INDICATORCLEARRANGE, 0, editor.Length)
+end
+
+local function MarkText(start, length)
+	scite.SendEditor(SCI_INDICATORFILLRANGE, start, length)
 end
 
 local sText = props['CurrentSelection']
@@ -32,7 +26,7 @@ if (sText == '') then
 end
 if string.len(sText) > 0 then
 	editor:MarkerDeleteAll(1)
-	UnderlineText(0, -1)
+	RemoveFindMarks()
 	if flag == SCFIND_WHOLEWORD then
 		print('> Поиск слова: "'..sText..'"')
 	else
@@ -44,7 +38,7 @@ if string.len(sText) > 0 then
 		local m = editor:LineFromPosition(s) - 1
 		while s do
 			local l = editor:LineFromPosition(s)
-			UnderlineText(s, e-s)
+			MarkText(s, e-s)
 			count = count + 1
 			if l ~= m then
 				local str = string.gsub(' '..editor:GetLine(l),'%s+',' ')
@@ -60,7 +54,7 @@ if string.len(sText) > 0 then
 	end
 else
 	editor:MarkerDeleteAll(1)
-	UnderlineText(0, -1)
+	RemoveFindMarks()
 	print('> Сначала выделите в редакторе текст, который необходимо найти! (поиск текста)\n> Можно просто установить курсор на нужное слово (поиск слова)\n> Так же можно выделить текст в окне консоли')
 end
 --~ editor:CharRight() editor:CharLeft() --Снимает выделение с исходного текста
