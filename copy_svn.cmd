056756532d8cb6b1154ae268337b264c789ed7d4
mozers <mozers@localhost>
2008-03-18 20:57:38 +0000

    -chg: ??????? ???????????? ??????? del_svn.cmd ?? ?????????? copy_svn.cmd

---
+++ copy_svn.cmd
@@ -0,0 +1,13 @@
+@ECHO off
+CHCP 1251>nul
+ECHO Копирование каталога TRUNK в указанное место (без служебных подкаталогов .svn)
+ECHO ------------------------------------------------------------------------------
+ECHO .
+CD /D %~dp0
+SET /P dest=Куда скопировать? [По умолчанию - в C:\TEMP\TRUNK\]:
+IF "%dest%"=="" SET dest=C:\TEMP\TRUNK\
+ECHO \.svn\>exlist
+ECHO exlist>>exlist
+ECHO copy_svn.cmd>>exlist
+XCOPY %~dp0*.* %dest% /S /H /K /EXCLUDE:exlist
+DEL exlist
--- del_svn.cmd
+++
@@ -1,3 +0,0 @@
-@ECHO off
-FOR /R %~dp0 %%d IN (.svn\) DO RMDIR /S /Q %%d
-
