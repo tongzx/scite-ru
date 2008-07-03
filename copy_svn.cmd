6390b23bd9c3fd79bdfa7380a0533677b2bb7946
mozers <mozers@gmail.com>
2008-07-03 14:11:48 +0000

    -chg: ????????? ?????? ????????? ? ?????? python.properties, shell_rus.html ? ??.

--- copy_svn.cmd
+++ copy_svn.cmd
@@ -1,13 +1,13 @@
 @ECHO off
 ECHO Копирование каталога TRUNK в указанное место (без служебных подкаталогов .svn)
 ECHO ------------------------------------------------------------------------------
-ECHO .
+ECHO.
 CD /D %~dp0
 SET /P dest=Куда скопировать? [По умолчанию - в C:\TEMP\TRUNK\]:
 IF "%dest%"=="" SET dest=C:\TEMP\TRUNK\
 ECHO \.svn\>exlist
 ECHO exlist>>exlist
 ECHO copy_svn.cmd>>exlist
-XCOPY %~dp0*.* %dest% /S /H /K /EXCLUDE:exlist
+XCOPY "%~dp0*.*" "%dest%" /S /H /K /EXCLUDE:exlist
 DEL exlist
 
