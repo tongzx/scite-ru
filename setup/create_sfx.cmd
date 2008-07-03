0356fd4c45e5bfe8001e2dd31fad3111c8771dd3
mozers <mozers@gmail.com>
2008-07-03 14:15:48 +0000

    -chg: ???????? RAR SFX ?????????? SciTE-Ru

--- setup/create_sfx.cmd
+++ setup/create_sfx.cmd
@@ -1,23 +1,24 @@
 @ECHO OFF
-SET cur_path=%~dp0
+:: -------------------------------------------
+SET sfx_filename=SciTE_Setup
+SET distrib_path=..\pack
 SET RAR="%ProgramFiles%\WinRAR\WinRAR.exe"
-SET name=SciTE_Setup
-CD %cur_path%
+:: -------------------------------------------
 
-PUSHD ..\pack
-%RAR% a -r -rr -s -m5 "%cur_path%%name%"
+SET cur_path=%~dp0
+CD /D "%cur_path%"
+
+PUSHD "%distrib_path%"
+%RAR% a -r -rr -s -m5 "%cur_path%%sfx_filename%"
 IF ERRORLEVEL 1 GOTO error
 POPD
 
-%RAR% SSciTE.sfx %name%.rar -Ztext.html -ISciBall.ico
+%RAR% SSciTE.sfx %sfx_filename%.rar -Ztext.html -iiconc:SciBall.ico
 IF ERRORLEVEL 1 GOTO error
 
-DEL %name%.rar
-
-GOTO end
+DEL %sfx_filename%.rar
+GOTO :EOF
 
 :error
 ECHO Error create SFX file!
 PAUSE
-
-:end
