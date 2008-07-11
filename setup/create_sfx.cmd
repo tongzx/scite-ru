a7a3da4a9fbbb6628652b41f9ee8322d1a406861
mozers <mozers@localhost>
2008-07-11 19:21:19 +0000

    -chg: SciTE_WinIntegrator.hta ?????? ?????? ? ???? ?????????? (???? ? SciTE).       ????????????? ??????????? ?? ????????? ????????? SciTE.

--- setup/create_sfx.cmd
+++ setup/create_sfx.cmd
@@ -7,11 +7,13 @@ SET RAR="%ProgramFiles%\WinRAR\WinRAR.exe"
 
 SET cur_path=%~dp0
 CD /D "%cur_path%"
+REN "%distrib_path%\locale.properties" "locale_rus.properties"
 
 PUSHD "%distrib_path%"
 %RAR% a -r -rr -s -m5 "%cur_path%%sfx_filename%"
 IF ERRORLEVEL 1 GOTO error
 POPD
+REN "%distrib_path%\locale_rus.properties" "locale.properties"
 
 %RAR% SSciTE.sfx %sfx_filename%.rar -Ztext.html -iiconc:SciBall.ico
 IF ERRORLEVEL 1 GOTO error
