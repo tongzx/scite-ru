d700bc76b27663775f78eb0db20afdc120ca24fc
ur4ltz <ur4ltz@localhost>
2008-09-22 13:07:27 +0000

    -add: ??????? ?????? ????? ????????? "scite.la" "libscite.a" ????????? ????? ???? ???? "pexports.exe" ? ??????????? ?????? ?????? MinGW ?? ???????. http://scite-ru.googlecode.com/files/pexports.zip SCITE.EXE ?? ?????? ???? ???????? UPX!

---
+++ lualib/lib/make.cmd
@@ -0,0 +1,4 @@
+@ECHO OFF
+SET PATH=C:\MinGW\bin;%ProgramFiles%\CodeBlocks\bin
+
+mingw32-make
--- lualib/winreg/make.cmd
+++ lualib/winreg/make.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-SET PATH=C:\MinGW\bin;%PATH%;
+SET PATH=C:\MinGW\bin;%ProgramFiles%\CodeBlocks\bin;%PATH%;
 
 mingw32-make all
 mingw32-make clean
