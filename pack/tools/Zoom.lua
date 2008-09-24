31f39420f5d6755e304b2d61fed4f8c15d3cc49f
mozers <mozers@gmail.com>
2008-09-24 12:55:55 +0000

    -chg: Zoom.lua: ?????????? ?????? ? ??????????? ??????? ?????? ? ???????.

--- pack/tools/Zoom.lua
+++ pack/tools/Zoom.lua
@@ -1,24 +1,29 @@
--- Zoom.lua
--- Version: 1.1
--- Author: Дмитрий Маслов
----------------------------------------------------
--- Обработка стандартной команды Zoom
--- Вместе с отображаемыми шрифтами, масштабируется и выводимый на принтер шрифт
--- Изменяет значение пользовательской переменной font.current.size, используемой для отображения текщего размера шрифта в строке состояния
----------------------------------------------------
--- Для подключения добавьте в файл .properties:
---   statusbar.text.1=$(font.current.size)px
--- в файл SciTEStartup.lua:
---   dofile (props["SciteDefaultHome"].."\\tools\\Zoom.lua")
----------------------------------------------------
+--[[-------------------------------------------------
+Zoom.lua
+Version: 1.2
+Authors: mozers™, Дмитрий Маслов
+-----------------------------------------------------
+Обработка стандартной команды Zoom
+Вместе с отображаемыми шрифтами, масштабируется и выводимый на принтер шрифт
+Изменяет значение пользовательской переменной font.current.size, используемой для отображения текщего размера шрифта в строке состояния
+Изменяет значения параметров (magnification, print.magnification, output.magnification) сохраняемых с помощью save_settings.lua
+-----------------------------------------------------
+Для подключения добавьте в файл .properties:
+  statusbar.text.1=$(font.current.size)px
+в файл SciTEStartup.lua:
+  dofile (props["SciteDefaultHome"].."\\tools\\Zoom.lua")
+--]]-------------------------------------------------
 
 local function ChangeFontSize(zoom)
-	props["magnification"] = zoom
-	props["print.magnification"] = zoom
-	props["output.magnification"] = zoom -- правильней было бы менять по SCI_SETZOOM в окне консоли, но как отловить это событие?
-	local _, _, font_current_size = string.find(props["style.*.32"], "size:(%d+)")
-	props["font.current.size"] = font_current_size + zoom -- Used in statusbar
-	scite.UpdateStatusBar()
+	if output.Focus then
+		props["output.magnification"] = output.Zoom
+	else
+		props["magnification"] = zoom
+		props["print.magnification"] = zoom
+		local _, _, font_current_size = props["style.*.32"]:find("size:(%d+)")
+		props["font.current.size"] = font_current_size + zoom -- Used in statusbar
+		scite.UpdateStatusBar()
+	end
 end
 
 -- Добавляем свой обработчик события OnSendEditor
@@ -27,7 +32,7 @@ function OnSendEditor(id_msg, wp, lp)
 	local result
 	if old_OnSendEditor then result = old_OnSendEditor(id_msg, wp, lp) end
 	if id_msg == SCI_SETZOOM then
-		if ChangeFontSize(lp) then return true end
+		ChangeFontSize(lp)
 	end
 	return result
 end
