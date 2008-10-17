4f1613987f596c169840ddbfd37bcf1c240c6866
mozers <mozers@localhost>
2008-10-17 21:41:43 +0000

    -chg: rename.lua: ?????????? ?????????. ?????????? ??????.

--- pack/tools/rename.lua
+++ pack/tools/rename.lua
@@ -1,6 +1,6 @@
 --[[--------------------------------------------------
  Rename.lua
- Version: 2.0.1
+ Version: 2.1
  Author: mozersЩ (иде€ codewarlock1101)
  ------------------------------------------------
  ѕереименовывает текущий файл
@@ -15,11 +15,19 @@ function CheckFilename(char)
 end
 
 local filename = props["FileNameExt"]
-local filename_new = shell.inputbox("Rename", "Enter new file name:", filename, "CheckFilename")
-if filename_new == nil then return end
-if filename_new.len ~= 0 and filename_new ~= filename then
-	local file_dir = props["FileDir"]
-	filename_new = file_dir:gsub('\\','\\\\')..'\\\\'..filename_new
-	scite.Perform("saveas:"..filename_new)
-	os.remove(file_dir..'\\'..filename)
-end
+local filename_new = props["FileNameExt"]
+local title = scite.GetTranslation("File Rename")
+local msg1 = scite.GetTranslation("Please enter new file name:")
+local msg2 = scite.GetTranslation("The file with such name already exists!\nPlease enter different file name.")
+repeat
+	filename_new = shell.inputbox(title, msg1, filename_new, "CheckFilename")if filename_new == nil then return end
+	if #filename_new == 0 then return end
+	if filename_new == filename then return end
+	if not shell.fileexists(filename_new) then break end
+	shell.msgbox(msg2, title)
+until false
+
+local file_dir = props["FileDir"]
+filename_new = file_dir:gsub('\\','\\\\')..'\\\\'..filename_new
+scite.Perform("saveas:"..filename_new)
+os.remove(file_dir..'\\'..filename)
