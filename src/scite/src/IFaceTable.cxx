7560530579b5f08761827ec0aa2cac79722e0781
vladvro <vladvro@localhost>
2008-11-27 13:00:24 +0000

    -chg: {LexPascal} ???????? ?????? ??????? (???????? ?????? ? ??? ??????)

--- src/scite/src/IFaceTable.cxx
+++ src/scite/src/IFaceTable.cxx
@@ -1140,6 +1140,21 @@ static IFaceConstant ifaceConstants[] = {
 	{"SCE_OPAL_SORT",5},
 	{"SCE_OPAL_SPACE",0},
 	{"SCE_OPAL_STRING",6},
+	{"SCE_PAS_ASM",14},
+	{"SCE_PAS_CHARACTER",12},
+	{"SCE_PAS_COMMENT",2},
+	{"SCE_PAS_COMMENT2",3},
+	{"SCE_PAS_COMMENTLINE",4},
+	{"SCE_PAS_DEFAULT",0},
+	{"SCE_PAS_HEXNUMBER",8},
+	{"SCE_PAS_IDENTIFIER",1},
+	{"SCE_PAS_NUMBER",7},
+	{"SCE_PAS_OPERATOR",13},
+	{"SCE_PAS_PREPROCESSOR",5},
+	{"SCE_PAS_PREPROCESSOR2",6},
+	{"SCE_PAS_STRING",10},
+	{"SCE_PAS_STRINGEOL",11},
+	{"SCE_PAS_WORD",9},
 	{"SCE_PLM_COMMENT",1},
 	{"SCE_PLM_CONTROL",6},
 	{"SCE_PLM_DEFAULT",0},
@@ -2429,7 +2444,7 @@ static IFaceProperty ifaceProperties[] = {
 
 enum {
 	ifaceFunctionCount = 247,
-	ifaceConstantCount = 1905,
+	ifaceConstantCount = 1920,
 	ifacePropertyCount = 137
 };
 
