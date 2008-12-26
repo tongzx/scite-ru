a3734e345c5610edd2281cfa5ac4312ec61ad085
mozers <mozers@localhost>
2008-12-26 22:04:15 +0000

    -chg: ???????? ?????? OpenFindFiles.lua

--- pack/tools/OpenFindFiles.lua
+++ pack/tools/OpenFindFiles.lua
@@ -1,5 +1,5 @@
 -- OpenFindFiles.lua
--- Version: 1.1
+-- Version: 1.2
 -- Author: mozers™
 ---------------------------------------------------
 -- После выполнения команды "Найти в файлах..."
@@ -8,60 +8,57 @@
 -- В файл SciTEStartup.lua добавьте строку:
 --   dofile (props["SciteDefaultHome"].."\\tools\\OpenFindFiles.lua")
 ---------------------------------------------------
-local command_name = "Открыть найденные файлы"
--- local command_name = "Оpen Find Files"
-local user_outputcontext_menu = props["user.outputcontext.menu"]
-local clear_before_execute = props["clear.before.execute"]
-local command_num = 0
-
---------------------------------------------------
--- Открытие файлов, перечисленных в консоли
-function OpenFindFiles()
-	output_text = output:GetText()
-	local _, e, str = string.find (output_text, '"(.-)"')
-	local _, e, path = string.find (output_text, '"([^\r\n]+)\\', e+1)
-	local filename_old = ""
-	repeat
-		e = e + 1
-		local _, e, filename = string.find (output_text, '%.(\\.-):%d+:', e)
-		if filename ~= nil then
-			if filename ~= filename_old then
-				local path_filename = string.gsub(path..filename,'\\','\\\\')
-				local cmd = '"'..props["SciteDefaultHome"]..'\\scite.exe" -check.if.already.open=1 "-open:'..path_filename..'" "-find:'..str..'"'
-				shell.exec(cmd, nil, true, false)
-				filename_old = filename 
-			end
-		end
-	until e == nil
-	-- Восстанавливаем исходные значения всех измененных параметров
-	props["clear.before.execute"] = clear_before_execute
-	props["user.outputcontext.menu"] = user_outputcontext_menu
-	props["command."..command_num..".*"] = ""
-end
+local user_outputcontext_menu = props["user.outputcontext.menu.*"] -- исходное контекстное меню консоли
+local clear_before_execute = props["clear.before.execute"]         -- исходное значение параметра clear.before.execute
+local outputcontextmenu_changed = false -- признак модификации контекстного меню
+local command_num                       -- номер команды "OpenFindFiles" в меню Tools
 
 --------------------------------------------------
 -- Поиск незанятого пункта меню Tools
-local function command_number()
+local function GetFreeCommandNumber()
+	local IDM_TOOLS = 9000
 	for i = 20, 299 do
-		if props["command."..i..".*"] == "" then return i end
+		if props["command."..i..".*"] == "" then return IDM_TOOLS+i end
 	end
 end
 
 --------------------------------------------------
 -- Создание команды в меню Tools и вставка ее в контекстное меню консоли
 local function CreateMenu()
-	command_num = command_number()
+	command_num = GetFreeCommandNumber()
 	props["command."..command_num..".*"] = "OpenFindFiles"
 	props["command.mode."..command_num..".*"] = "subsystem:lua,savebefore:no"
-	props["user.outputcontext.menu"] = command_name.."|"..(9000+command_num).."|||"..user_outputcontext_menu
+
+	local command_name = scite.GetTranslation("Open Find Files")
+	props["user.outputcontext.menu.*"] = command_name.."|"..command_num.."|||"..user_outputcontext_menu
+
+	outputcontextmenu_changed = true
+	props["clear.before.execute"] = 0
 end
 
 --------------------------------------------------
-local function MenuCommand(msg)
-	if msg == IDM_FINDINFILES then
-		props["clear.before.execute"] = 0
-		CreateMenu()
+-- Удаление команды из меню Tools и восстановление исходного контекстного меню консоли
+local function RemoveMenu()
+	props["clear.before.execute"] = clear_before_execute
+	props["user.outputcontext.menu.*"] = user_outputcontext_menu
+	props["command."..command_num..".*"] = ""
+	outputcontextmenu_changed = false
+end
+
+--------------------------------------------------
+-- Открытие файлов, перечисленных в консоли
+function OpenFindFiles()
+	local output_text = editor:GetText()
+	local str = output_text:match('"(.-)"')
+	local filename_prev = ''
+	for filename in output_text:gmatch('([^\r\n]+):%d+:') do
+		if filename ~= filename_prev then
+			scite.Open(filename)
+			editor:GotoPos(editor:findtext(str))
+			filename_prev = filename
+		end
 	end
+	RemoveMenu()
 end
 
 --------------------------------------------------
@@ -70,6 +67,10 @@ local old_OnMenuCommand = OnMenuCommand
 function OnMenuCommand (msg, source)
 	local result
 	if old_OnMenuCommand then result = old_OnMenuCommand(msg, source) end
-	MenuCommand(msg)
+	if msg == IDM_FINDINFILES then
+		CreateMenu()
+	elseif outputcontextmenu_changed and msg ~= command_num then
+		RemoveMenu()
+	end
 	return result
 end
