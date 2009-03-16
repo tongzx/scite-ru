aa198c889f2fbb2608d710d3a6df947b3830caac
mozers <mozers@gmail.com>
2009-03-16 09:09:53 +0000

    -chg: FuncProcList.lua ????????? ? ????????? (??? ?????? ??? ????????????, ????? ???? ?? ??????? ????? ?????????????? ??????? ? SideBar)       UTF8_check.lua ????????? ? ????????? (????????????? ?????? ??????? ????? ?? ??? ?????).

--- pack/tools/UTF8_check.lua
+++ pack/tools/UTF8_check.lua
@@ -1,52 +1,39 @@
 --[[----------------------------------------------------------------------------
 UTF8_check.lua
-Author: VladVRO
-version 1.1
+Author: VladVRO, Tymur Gubayev
+version 2.0
 
 Автоматическое переключение кодировки в UTF-8
-для html файлов содержащих "Content-Type: text/html; charset=UTF-8"
-и для файлов содержащих символы русского алфавита в UTF-8 кодировке
+для файлов содержащих символы русского алфавита в UTF-8 кодировке
 
 Подключение:
 В файл SciTEStartup.lua добавить строку:
 	dofile ("UTF8_check.lua")
 В файле настроек добавить:
 	utf8.check=1
-
-Дополнительно:
-есть возможность ограничить зону поиска в файле начальным блоком задав в настройках:
-	utf8.check.max=<кол-во байт>
-это позволит быстрее открывать большие по размеру файлы, но снизит вероятность
-правильного определения кодировки.
 --]]----------------------------------------------------------------------------
 
+-- Author: VladVRO (менее точен, но не требует внешних библиотек)
 local function utf8_check ()
-	if props["utf8.check"] == "1" then
-		local maxpos = tonumber(props["utf8.check.max"])
-		if not maxpos then maxpos = editor.Length end
-		-- html content-type
-		if string.find(props["file.patterns.html"], props["FileExt"]) ~= nil then
-			if editor:findtext("Content-Type: text/html; charset=UTF-8", SCFIND_POSIX, 0, maxpos) then
-				scite.MenuCommand(IDM_ENCODING_UCOOKIE)
-				return
-			end
-		end
-		-- by russian alphabet
-		local pattern = "[РС]"
-		local pos = editor:findtext(pattern, SCFIND_REGEXP, 0)
-		while pos do
-			local c0 = editor.CharAt[pos]
-			if c0 < 0 then c0 = c0 + 256 end
-			local c1 = editor.CharAt[pos+1]
-			if c1 < 0 then c1 = c1 + 256 end
-			if (c0 == 208 and c1 >= 144 and c1 <= 175) or
-				 (c0 == 209 and c1 >= 128 and c1 <= 145)
-			then
-				scite.MenuCommand(IDM_ENCODING_UCOOKIE)
-				return
-			end
-			pos = editor:findtext(pattern, SCFIND_REGEXP, pos+1, maxpos)
-		end
+	-- by russian alphabet
+	if editor:findtext("[\208\209][\128-\191][\208\209][\128-\191]", SCFIND_REGEXP, 0) then
+		scite.MenuCommand(IDM_ENCODING_UCOOKIE)
+	end
+end
+
+-- Author: Tymur Gubayev
+local function IsUTF()
+	require 'lpeg'
+	local text = editor:GetText()
+	local cont = lpeg.R("\128\191")   -- continuation byte
+	local utf8 = lpeg.R("\0\127")^1
+			+ (lpeg.R("\194\223") * cont)^1
+			+ (lpeg.R("\224\239") * cont * cont)^1
+			+ (lpeg.R("\240\244") * cont * cont * cont)^1
+	local latin = lpeg.R("\0\127")^1
+	local searchpatt = latin^0 * utf8 ^1 * -1
+	if searchpatt:match(text) then
+		scite.MenuCommand(IDM_ENCODING_UCOOKIE)
 	end
 end
 
@@ -55,10 +42,11 @@ local old_OnOpen = OnOpen
 function OnOpen (filename)
 	local result
 	if old_OnOpen then result = old_OnOpen(filename) end
-	
-	if editor.CodePage ~= SC_CP_UTF8 then
-		utf8_check()
+	if props["utf8.check"] == "1" then
+		if editor.CodePage ~= SC_CP_UTF8 then
+			-- utf8_check()
+			IsUTF()
+		end
 	end
-	
 	return result
 end
