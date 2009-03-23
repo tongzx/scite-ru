79605d7f2124cbe4e5a592b8986cf0f19b03d204
mozers <mozers@localhost>
2009-03-23 18:22:27 +0000

    -fix: ????? ????????? ??????? ur4ltz ??? ?????????. ?????, ??? ??? - ????????? ????????????? ?? ????? ?????? ????? ??????.

--- iconlib/gnome/make.cmd
+++ iconlib/gnome/make.cmd
@@ -18,7 +18,7 @@ ECHO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 CD /D "%~dp0"
 windres -o resfile.o toolbar.rc
 IF ERRORLEVEL 1 GOTO error
-ld --nmagic --strip-all --entry=0 --dll -o gnome.dll resfile.o
+gcc -s -shared -nostdlib -o gnome.dll resfile.o
 IF ERRORLEVEL 1 GOTO error
 
 DEL resfile.o
