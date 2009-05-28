0fe3b9825f7d0a82dafd6b85187afa2504b32389
mozers <mozers@localhost>
2009-05-28 17:44:25 +0000

    -fix: RestoreRecent.js: ?????????? ????????? ??????????? ?????? ?? ?? ??????? ? SciTE.recent ?????????? ?? ??? ?????? ?? ??????????? ??????. ?????? ????????? ??????. ??? ???? ????? ?????????.

--- pack/tools/RestoreRecent.js
+++ pack/tools/RestoreRecent.js
@@ -1,92 +1,32 @@
 /*
 RestoreRecent.js
 Authors: mozers™
-Version: 1.3
+Version: 1.5
 ------------------------------------------------------
 Description:
-  It is started from script RestoreRecent.lua
+  It is started on close SciTE (from RestoreRecent.lua)
   Save position, bookmarks, folds to SciTE.recent file
-  Запускается из скрипта RestoreRecent.lua
+  Запускается при закрытии SciTE из скрипта RestoreRecent.lua
   Читает позицию курсора, букмарки и фолдинг из файла SciTE.session и дополняет ими файл SciTE.recent
 */
+var link_age_max = 7; // срок жизни ссылок в SciTE.recent (дней)
 
 var WshShell = new ActiveXObject("WScript.Shell");
 var FSO = new ActiveXObject("Scripting.FileSystemObject");
 
-var params = ['date', 'path', 'position', 'bookmarks', 'folds'];
-var cur_date, cur_date_string, recent_arr;
-
-try {
-	var scite_user_home = WScript.Arguments(0); // этот параметр передается в ком.строке родительского скрипта (RestoreRecent.lua)
-} catch(e) {
-	WScript.Echo('This script started only from RestoreRecent.lua!');
-	WScript.Quit(1);
-}
-var session_filename = scite_user_home + '\\SciTE.session';
-var recent_filename = scite_user_home + '\\SciTE.recent';
-
-// Возвращает массив, содержащий дату {d, m, y}
-function GetDate(obj) {
-	var d = new Date(obj);
-	if (obj===undefined) {d = new Date();}
-	var arr = [d.getDate(), d.getMonth(), d.getYear()];
-	return arr;
-}
-
-// Преобразует дату к виду "dd.mm.yyyy"
-function DateFormatString(date_arr){
-	var d = date_arr[0];
-	d = (d < 10) ? ('0' + d) : d;
-	var m = date_arr[1] + 1;
-	m = (m < 10) ? ('0' + m) : m;
-	var y = date_arr[2];
-	return d + '.' + m + '.' + y;
-}
-
-// Преобразует дату к числу (посольку +-1день не слишком важно, то сойдет и так)
-function DateFormatNumber(date_arr){
-	var d = date_arr[0];
-	var m = date_arr[1] * 30;
-	var y = date_arr[2] * 365;
-	return d + m + y;
-}
-
-// Преобразование строки даты "dd.mm.yyyy" к числу
-function DateString2Number(date_str){
-	var m = date_str.match(/(\d+)\.(\d+)\.(\d+)/);
-	var arr = [Number(m[1]), Number(m[2])-1, Number(m[3])];
-	return DateFormatNumber(arr);
-}
-
-// Преобразование строкового имени параметра в числовой индекс
-function Param2Index(param){
-	for (var i=0; i<params.length; i++) {
-		if (param == params[i]) {return i;}
-	}
-	return -1;
-}
-
-// Читаем SciTE.recent в двухмерный массив recent_arr[номер_файла][имя_параметра] = значение
+// Читаем SciTE.recent в массив объектов, каждый из которых имеет свойства date, path, position, bookmarks и folds
 function ReadRecentFile(filename) {
 	var arr = [];
 	if (FSO.FileExists(filename)) {
 		if (FSO.GetFile(filename).Size > 0) {
-			file = FSO.OpenTextFile(filename, 1);
+			var file = FSO.OpenTextFile(filename, 1);
 			while (!file.AtEndOfStream){
 				var line = file.ReadLine();
-				var r = line.match(/buffer\.(\d)\.([a-z]+)=(.+)$/);
+				var r = line.match(/buffer\.(\d+)\.([a-z]+)=(.+)$/);
 				if (r) { // r = массив: {1-номер_файла, 2-имя_параметра, 3-значение}
-					var y = Param2Index(r[2]);
-					if (y != -1) {
-						var x = r[1];
-						if (!arr[x]) {
-							var arr_tmp = [];
-							arr_tmp[y] = r[3];
-							arr[x] = arr_tmp;
-						}else{
-							arr[x][y] = r[3];
-						}
-					}
+					var link_num = r[1];
+					if (!arr[link_num]) arr[link_num] = {};
+					arr[link_num][r[2]] = r[3];
 				}
 			}
 			file.Close();
@@ -95,32 +35,43 @@ function ReadRecentFile(filename) {
 	return(arr);
 }
 
-// Проверка наличия в массиве записи с такими же данными (recent_arr[i][1] == имя_файла ?)
-function IsRecent(filespec){
-	for (var i=1; i<recent_arr.length; i++) {
-		if (recent_arr[i][1].toLowerCase() == filespec.toLowerCase()) {break;}
-	}
-	// если запись о файле существует - уничтожаем все прежние данные о нем
-	// если запись отсутсвует - создаем ее и возвращаем новый размер массива
-	var arr_tmp = [];
-	arr_tmp[0] = cur_date_string;
-	recent_arr[i] = arr_tmp;
-	return i;
-}
-
 // Читаем SciTE.session в массив, проверяя наличие в нем аналогичных записей
 function ReadSessionFile(filename){
+	// Return date as string 'dd.mm.yyyy'
+	function DateString(){
+		function format(x){return (x < 10) ? ('0' + x) : String(x);}
+		var d = new Date();
+		var year = String(d.getYear());
+		var month = format(d.getMonth()+1);
+		var day = format(d.getDate());
+		return day + '.' + month + '.' + year;
+	}
+
+	// Проверка наличия в массиве записи с такими же данными (recent_arr[i][1] == имя_файла ?)
+	function IsRecent(filespec){
+		for (var i=1; i<recent_arr.length; i++) {
+			if (recent_arr[i].path.toLowerCase() == filespec.toLowerCase()) {
+				break;
+			}
+		}
+		// если запись о файле существует - удаляем все прежние данные о ней
+		// если запись отсутсвует - создаем ее и возвращаем новый размер массива
+		recent_arr[i] = {};
+		recent_arr[i].date = cur_date_string;
+		return i;
+	}
+
 	if (FSO.FileExists(filename)) {
 		if (FSO.GetFile(filename).Size > 0) {
-			file = FSO.OpenTextFile(filename, 1);
+			var file = FSO.OpenTextFile(filename, 1);
+			var cur_date_string = DateString();
 			var x;
 			while (!file.AtEndOfStream) {
 				var line = file.ReadLine();
-				var r = line.match(/buffer\.(\d)\.([a-z]+)=(.+)$/);
+				var r = line.match(/buffer\.(\d+)\.([a-z]+)=(.+)$/);
 				if (r) { // r = массив: {1-номер_файла, 2-имя_параметра, 3-значение}
-					if (r[2] == 'path') {x = IsRecent(r[3]);}
-					var y = Param2Index(r[2]);
-					recent_arr[x][y] = r[3];
+					if (r[2] == 'path') x = IsRecent(r[3]);
+					if (r[2] != 'current') recent_arr[x][r[2]] = r[3];
 				}
 			}
 			file.Close();
@@ -128,31 +79,52 @@ function ReadSessionFile(filename){
 	}
 }
 
-// Удаляем из массива записи о файлах в которых пользователь даже курсор не сдвинул.
+// Удаляем мусор из массива
+// (записи о файлах в которых пользователь даже курсор не сдвинул и, раз в день - старые ссылки)
 function RemoveWaste(){
+	// Это сегодняшний SciTE.recent ?
+	function IsTodaysRecent(){
+		if (FSO.FileExists(recent_filename)) {
+			var recent_date = FSO.GetFile(recent_filename).DateLastModified;
+			var current_date = new Date();
+			var date_diff = new Date(current_date - recent_date);
+			if (parseInt(date_diff/86400000) === 0) return true;
+		}
+		return false;
+	}
+
+	// Определение возраста ссылки
+	function LinkAge(link_date_string){
+		var arrDate = link_date_string.match(/(\d+)\.(\d+)\.(\d+)/);
+		var current_date = new Date();
+		var link_date = new Date(arrDate[3], arrDate[2]-1, arrDate[1]);
+		var date_diff = new Date(current_date.valueOf() - link_date.valueOf());
+		return parseInt(date_diff/86400000);
+	}
+
+	// Определение кол-ва не-null-евых параметров в объекте
+	function CountParams(arr){
+		var count = 0;
+		for (var i in arr){
+			if (arr[i]) count++;
+		}
+		return count;
+	}
+
 	// Удаляем бессодержательные ссылки
-	for (var i=recent_arr.length-1; i>0; i--){
-		if ((recent_arr[i].length == 3) && (recent_arr[i][2] == 1)) {
+	for (var i in recent_arr){
+		if ((CountParams(recent_arr[i]) == 3) && (recent_arr[i].position == 1)) {
 		// если в записи о файле только 3 параметра {дата,позиция,путь} и позиция=1 то:
-			recent_arr.splice(i, 1);
+			recent_arr[i] = null;
 		}
 	}
 
 	// Удаляем старые ссылки
-	var link_age = 30; // max срок жизни ссылок в SciTE.recent (дней)
-	var cur_date_number = DateFormatNumber(cur_date);
-	var recent_date_number = 0;
-	if (FSO.FileExists(recent_filename)) {
-		var recent_date = FSO.GetFile(recent_filename).DateLastModified;
-		recent_date_number = DateFormatNumber(GetDate(recent_date));
-	}
-	// Процедура очистки запускается только 1 раз в день
-	if (cur_date_number > recent_date_number) {
-		for (var j=recent_arr.length-1; j>0; j--){
-			var link_date_number = DateString2Number(recent_arr[j][0]);
-			if (cur_date_number - link_date_number > link_age) {
-			// если возраст линка больше указанного количества дней, то:
-				recent_arr.splice(j, 1);
+	if (!IsTodaysRecent()) { // Процедура очистки запускается только 1 раз в день
+		for (var j in recent_arr){
+			if (LinkAge(recent_arr[j].date) > link_age_max){
+				// если возраст линка больше указанного количества дней, то:
+				recent_arr[j] = null;
 			}
 		}
 	}
@@ -161,13 +133,12 @@ function RemoveWaste(){
 // Cохраняем массив в SciTE.recent
 function SaveRecentFile(filename){
 	var file = FSO.OpenTextFile(filename, 2, true);
-	for (var i=0; i<recent_arr.length; i++){
-		if (recent_arr[i]) {
-			for (var j=0; j<recent_arr[i].length; j++) {
-				var value = recent_arr[i][j];
-				if (value) {
-					file.WriteLine('buffer.' + i + '.' + params[j] + '=' + value);
-				}
+	var buf = 0;
+	for (var i in recent_arr){
+		if (recent_arr[i]){
+			buf++;
+			for (var params in recent_arr[i]) {
+				file.WriteLine('buffer.' + buf + '.' + params + '=' + recent_arr[i][params]);
 			}
 			file.WriteLine(''); // чиста для красаты :)
 		}
@@ -175,9 +146,17 @@ function SaveRecentFile(filename){
 	file.Close();
 }
 
-cur_date = GetDate();
-cur_date_string = DateFormatString(cur_date);
-recent_arr = ReadRecentFile(recent_filename);
+try {
+	var scite_user_home = WScript.Arguments(0); // этот параметр передается в ком.строке родительского скрипта (RestoreRecent.lua)
+} catch(e) {
+	WScript.Echo('This script started only from RestoreRecent.lua!');
+	WScript.Quit(1);
+}
+
+var session_filename = scite_user_home + '\\SciTE.session';
+var recent_filename = scite_user_home + '\\SciTE.recent';
+var recent_arr = ReadRecentFile(recent_filename);
+
 ReadSessionFile(session_filename);
 RemoveWaste();
 SaveRecentFile(recent_filename);
