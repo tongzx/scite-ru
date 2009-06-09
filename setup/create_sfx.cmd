bb20979f3e4ece43bc8b3b70d995997decec3710
mozers <mozers@localhost>
2009-06-09 16:41:21 +0000

    -chg: locale.properties ???????????? ? locale-ru.properties. ??????? ??????????????? ????????? ? SciTEGlobal.properties, SciTE_WinIntegrator.hta ? ?????? ??? ?????? ?????? (create_sfx.cmd).

--- setup/create_sfx.cmd
+++ setup/create_sfx.cmd
@@ -1,20 +1,16 @@
 @ECHO OFF
 :: -------------------------------------------
 SET sfx_filename=SciTE_Setup
-SET distrib_path=..\pack
 SET RAR="%ProgramFiles%\WinRAR\WinRAR.exe"
+SET cur_path=%~dp0
+SET distrib_path=%cur_path%..\pack
 :: -------------------------------------------
 
-SET cur_path=%~dp0
-CD /D "%cur_path%"
-REN "%distrib_path%\locale.properties" "locale_rus.properties"
-
-PUSHD "%distrib_path%"
+CD /D "%distrib_path%"
 %RAR% a -r -rr -s -m5 "%cur_path%%sfx_filename%"
 IF ERRORLEVEL 1 GOTO error
-POPD
-REN "%distrib_path%\locale_rus.properties" "locale.properties"
 
+CD /D "%cur_path%"
 %RAR% SSciTE.sfx %sfx_filename%.rar -Ztext.html -iiconc:SciBall.ico
 IF ERRORLEVEL 1 GOTO error
 
