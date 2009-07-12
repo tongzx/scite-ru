d7bbbcd4d99b3c3a4593b84b6884fceaeb0ac393
mozers <mozers@localhost>
2009-07-12 12:14:04 +0000

    -chg: ? ????????? ??? ?????????? iconlib ????????? ?????????????????? ??????? ????????????? ??????? (???? ?? ?????, ????? ?????? ????, ??? ?? - ? ?????, ?? ???????????? ???????????? ??????, ???????????? UR4LTZ)

--- iconlib/cool/make.cmd
+++ iconlib/cool/make.cmd
@@ -6,6 +6,7 @@ windres -o resfile.o toolbar.rc
 IF ERRORLEVEL 1 EXIT
 
 ld --strip-all --dll -o cool.dll resfile.o
+REM gcc -s -shared -o cool.dll resfile.o
 IF ERRORLEVEL 1 EXIT
 
 DEL resfile.o
--- iconlib/gnome/make.cmd
+++ iconlib/gnome/make.cmd
@@ -6,6 +6,7 @@ windres -o resfile.o toolbar.rc
 IF ERRORLEVEL 1 EXIT
 
 ld --strip-all --dll -o gnome.dll resfile.o
+REM gcc -s -shared -o gnome.dll resfile.o
 IF ERRORLEVEL 1 EXIT
 
 DEL resfile.o
