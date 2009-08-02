14eab61b539070c8aefaed8d4448db9088e83030
mozers <mozers@localhost>
2009-08-02 19:14:27 +0000

    -chg: highlighting_identical_text.lua: ?????? ????????? ? ?????? ????????? Issue 149: - ????? ???????? highlighting.identical.text.max ?????? max ???-?? ????????? - ????? ?????? find.mark.5 ?????? ????? ??????? ?????? ??? ?????????? max ???-?? ????????? - ?????? ?????? ????? ?????? ??????? ????? (? ?? ?????? ?????????) ? UTF ???????

--- pack/tools/highlighting_identical_text.lua
+++ pack/tools/highlighting_identical_text.lua
@@ -1,6 +1,6 @@
 --[[--------------------------------------------------
 Highlighting Identical Text
-Version: 1.1.1
+Version: 1.2
 Author: mozers™
 ------------------------------
 Авто подсветка текста, который совпадает с текущим словом или выделением
@@ -20,72 +20,56 @@ Author: mozers™
 	command.139.*=highlighting_identical_text_switch
 	command.mode.139.*=subsystem:lua,savebefore:no
 
-Дополнительно можно задать стиль используемого маркера (4):
-	find.mark.4=#FF66FF,box
+Задать стиль используемых маркеров
+(4 - отметка идентичного текста, 5 - отметка текста при превышении max кол-ва вхождений):
+	find.mark.4=#CC99FF,box
+	find.mark.5=#FF0000,box
+
+Дополнительно можно изменить значение по умолчанию (50) на max кол-во вхождений (0 - без ограничения).
+	highlighting.identical.text.max=100
 
 ============================================================================
 Если бы функция editor:findtext не вешала редактор при работе с UTF текстами,
 то код можно было бы значитально оптимизировать (см. версию 1.0 этого скрипта).
-Теперь кода почти в 2 раза больше и поиск русских слов в UTF не работает (только выделенный текст) :(
 ============================================================================
 --]]----------------------------------------------------
 
-local table_limit = 50 -- max кол-во результатов поиска (не рекомендуется ставить много)
-local store_pos    -- переменная для хранения передыдущей позиции курсора
-local store_text   -- переменная для хранения передыдущего текста
-local mark_num = 4 -- номер используемого маркера
-local all_text     -- текст текущего документа
-local chars_count  -- кол-во символов в текущем документе
+local count_max = 50   -- max кол-во результатов поиска (по умолчанию)
+local store_pos        -- переменная для хранения передыдущей позиции курсора
+local store_text       -- переменная для хранения передыдущего поискового текста
+local mark_ident = 4   -- номер маркера для отметки идентичного текста/слова
+local mark_max = 5     -- номер маркера для отметки при превышении max кол-ва вхождений
+local chars_count      -- кол-во символов в текущем документе
+local all_text         -- текст текущего документа
+
+local max = props['highlighting.identical.text.max']
+if max ~= '' then count_max = tonumber(max) end
 
 -- Переключатель подсветки (вкл/выкл) срабатывает из меню Tools
 function highlighting_identical_text_switch()
 	local prop_name = 'highlighting.identical.text'
 	props[prop_name] = 1 - tonumber(props[prop_name])
-	EditorClearMarks(mark_num)
+	EditorClearMarks(mark_ident)
 end
 
 local function IdenticalTextFinder()
 	local match_table = {}
+	local wholeword = false
 	local word_pattern
-
-	-- Поиск слов, идентичных текущему (если ничего не выделено)
-	function WordsMatch(find_word) -- Функция загоняет все результаты поиска в таблицу match_table
-		local find_start = 1
-		repeat
-			local ident_word_start, ident_word_end, ident_word = all_text:find(word_pattern, find_start)
-			if ident_word_start == nil then return end
-			if ident_word == find_word then
-				match_table[#match_table+1] = {ident_word_start-1, ident_word_end}
-				if #match_table > table_limit then -- если результатов больше, чем указанное число, то не показываем их
-					match_table = {}
-					return
-				end
-			end
-			find_start = ident_word_end + 1
-		until false
+	----------------------------------------------------------
+	-- Проверка, является ли найденный текст словом
+	function isWord(all_text, text_start, text_end)
+		if  ( (text_start==1)       or (all_text:sub(text_start-1, text_start-1):match(word_pattern)) ) and
+			( (text_end==#all_text) or (all_text:sub(text_end+1, text_end+1):match(word_pattern)) )    then
+				return true
+		end
+		return false
 	end
-
-	-- Поиск идентичного текста (если выделен текст)
-	function TextMatch(find_text) -- Функция загоняет все результаты поиска в таблицу match_table
-		local find_start = 1
-		repeat
-			local ident_text_start, ident_text_end = all_text:find(find_text, find_start, true)
-			if ident_text_start == nil then return end
-			match_table[#match_table+1] = {ident_text_start-1, ident_text_end}
-			if #match_table > table_limit then -- если результатов больше, чем указанное число, то не показываем их
-				match_table = {}
-				return
-			end
-			find_start = ident_text_end + 1
-		until false
-	end
-
 	----------------------------------------------------------
 	local current_pos = editor.CurrentPos
 	if current_pos == store_pos then return end
 	store_pos = current_pos
 
-	local wholeword = false
 	local cur_text = editor:GetSelText()
 	if cur_text == '' then
 		cur_text = GetCurrentWord()
@@ -94,17 +78,44 @@ local function IdenticalTextFinder()
 	if cur_text == store_text then return end
 	store_text = cur_text
 
-	EditorClearMarks(mark_num)
-	if wholeword then
-		word_pattern = '([' .. editor.WordChars .. ']+)'
-		WordsMatch(cur_text)
-	else
-		TextMatch(cur_text)
-	end
+	EditorClearMarks(mark_ident)
+	EditorClearMarks(mark_max)
+	if wholeword then word_pattern = '[^' .. editor.WordChars .. ']' end
+	----------------------------------------------------------
+	local find_start = 1
+	repeat
+		local ident_text_start, ident_text_end = all_text:find(cur_text, find_start, true)
+		if ident_text_start == nil then break end
+		if ( not wholeword ) or
+			( wholeword and isWord(all_text, ident_text_start, ident_text_end) ) then
+				-- загоняем все результаты поиска в таблицу match_table
+				match_table[#match_table+1] = {ident_text_start-1, ident_text_end}
+		end
+		if count_max ~= 0 then
+			if #match_table > count_max then -- если результатов больше, чем указанное число...
+				local err_start, err_end
+				if wholeword then
+					-- то маркируем только текущее слово
+					err_start = editor:WordStartPosition(current_pos, true)
+					err_end = editor:WordEndPosition(current_pos, true)
+					EditorMarkText(err_start, err_end-err_start, mark_max)
+					return
+				else
+					-- то маркируем только текущее выделение
+					err_start = editor.SelectionStart
+					err_end = editor.SelectionEnd
+					EditorMarkText(err_start, err_end-err_start, mark_max)
+					return
+				end
+			end
+		end
+		find_start = ident_text_end + 1
+	until false
+	----------------------------------------------------------
 	if #match_table > 1 then
 		for i = 1, #match_table do
 			-- Отмечаем все слова, беря данные из таблицы match_table
-			EditorMarkText(match_table[i][1], match_table[i][2]-match_table[i][1], mark_num)
+			EditorMarkText(match_table[i][1], match_table[i][2]-match_table[i][1], mark_ident)
 		end
 	end
 
