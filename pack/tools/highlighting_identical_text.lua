f8851dc4b1fda311d0b311ae62614731e611f1fa
mozers <mozers@localhost>
2009-08-05 11:23:29 +0000

    -chg: highlighting_identical_text.lua: - ?????? ?? ?????????/?????????? ??????? ????? ????? - ????? ???????? highlighting.identical.text.reserved.words.lexer ????????? ?????? ?????? ???????????????? ????. - ?????????? ????????? ?????? - ??? ??????? ?? ?????????? ? ???????????? VladVRO ? TymurGubayev

--- pack/tools/highlighting_identical_text.lua
+++ pack/tools/highlighting_identical_text.lua
@@ -1,6 +1,6 @@
 --[[--------------------------------------------------
 Highlighting Identical Text
-Version: 1.2.1
+Version: 1.3
 Author: mozers™
 ------------------------------
 Авто подсветка текста, который совпадает с текущим словом или выделением
@@ -27,6 +27,8 @@ Author: mozers™
 
 Дополнительно можно изменить значение по умолчанию (50) на max кол-во вхождений (0 - без ограничения).
 	highlighting.identical.text.max=100
+и задать перечень неподсвечиваемых слов для конкретного лексера (или для всех остальных - *)
+	highlighting.identical.text.reserved.words.lua=and,break,do,else,elseif,end,false,for,function,if,in,local,nil,not,or,repeat,return,then,true,until,while
 
 ============================================================================
 Если бы функция editor:findtext не вешала редактор при работе с UTF текстами,
@@ -41,6 +43,8 @@ local mark_ident = 4   -- номер маркера для отметки идентичного текста/слова
 local mark_max = 5     -- номер маркера для отметки при превышении max кол-ва вхождений
 local chars_count      -- кол-во символов в текущем документе
 local all_text         -- текст текущего документа
+local word_pattern     -- паттерн для поиска слов
+local reserved_words   -- неподсвечиваемые слова
 
 local max = props['highlighting.identical.text.max']
 if max ~= '' then count_max = tonumber(max) end
@@ -50,30 +54,39 @@ function highlighting_identical_text_switch()
 	local prop_name = 'highlighting.identical.text'
 	props[prop_name] = 1 - tonumber(props[prop_name])
 	EditorClearMarks(mark_ident)
+	store_pos, store_text = 0, ''
 end
 
-local function IdenticalTextFinder()
-	local match_table = {}
-	local wholeword = false
-	local word_pattern
-	----------------------------------------------------------
-	-- Проверка, является ли найденный текст словом
-	function isWord(all_text, text_start, text_end)
-		if  ( (text_start==1)       or (all_text:sub(text_start-1, text_start-1):match(word_pattern)) ) and
-			( (text_end==#all_text) or (all_text:sub(text_end+1, text_end+1):match(word_pattern)) )    then
-				return true
-		end
-		return false
+-- Проверка, является ли найденный текст словом
+local function isWord(all_text, text_start, text_end)
+	if  ( (text_start==1)       or (all_text:sub(text_start-1, text_start-1):match(word_pattern)) ) and
+		( (text_end==#all_text) or (all_text:sub(text_end+1, text_end+1):match(word_pattern)) )    then
+			return true
 	end
-	----------------------------------------------------------
+	return false
+end
+
+-- Проверка, является ли текущее слово зарезервированным
+local function isReservedWord(cur_text)
+	if reserved_words == '' then return false end
+	for w in string.gmatch(reserved_words, "%w+") do
+		if cur_text:lower() == w:lower() then return true end
+	end
+	return false
+end
+
+-- Поиск идентичных слов/текста
+local function IdenticalTextFinder()
 	local current_pos = editor.CurrentPos
 	if current_pos == store_pos then return end
 	store_pos = current_pos
 
+	local wholeword = false
 	local cur_text = editor:GetSelText()
 	if cur_text == '' then
 		cur_text = GetCurrentWord()
 		wholeword = true
+		if isReservedWord(cur_text) then return end
 	end
 	if cur_text == store_text then return end
 	store_text = cur_text
@@ -82,13 +95,14 @@ local function IdenticalTextFinder()
 	EditorClearMarks(mark_max)
 	if wholeword then word_pattern = '[^' .. editor.WordChars .. ']' end
 	----------------------------------------------------------
+	local match_table = {}
 	local find_start = 1
 	repeat
 		local ident_text_start, ident_text_end = all_text:find(cur_text, find_start, true)
 		if ident_text_start == nil then break end
 		if ident_text_end == 0 then break end
 		if ( not wholeword ) or
-			( wholeword and isWord(all_text, ident_text_start, ident_text_end) ) then
+			( isWord(all_text, ident_text_start, ident_text_end) ) then
 				-- загоняем все результаты поиска в таблицу match_table
 				match_table[#match_table+1] = {ident_text_start-1, ident_text_end}
 		end
@@ -132,6 +146,8 @@ function OnUpdateUI ()
 			if editor.Length ~= chars_count then
 				all_text = editor:GetText()
 				chars_count = editor.Length
+				reserved_words = props['highlighting.identical.text.reserved.words.' .. editor.LexerLanguage]
+				if reserved_words == '' then reserved_words = props['highlighting.identical.text.reserved.words.*'] end
 			end
 			IdenticalTextFinder()
 		end
