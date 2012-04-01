befd90d0cf53c1b294e48514bdf13fe92d0f5228
mozers <mozers@localhost>
2012-04-01 14:37:58 +0400

    -chg: svn_menu.lua: ???????: - ???? ????? ".svn"("_svn") ? ???????? ? ?.?. ???? ?? ?????? ??? ?????? ?? ?????; - ?????? ???? ?????? ???? ??? ??? ??????????? ? SVN ?????? ? ??? ?? ???????????.   ?????? ??????? subversion ??? ???? ? ??????? ????? ???????? ? ????? db. ?? ????   ??????????? ? ?????? ??? ???? ????? (? ?????????? ????? ? ??????, ??????, ??????) - ??????? ????? "Check for modification for all"; ?????: arhohryakov. Issue 288.

--- pack/tools/svn_menu.lua
+++ pack/tools/svn_menu.lua
@@ -1,8 +1,8 @@
 --[[
 Eng: Creating SVN commands submenu in tab context menu
 Rus: —оздает в контекстном меню таба (вкладки) подменю дл€ команд SVN
-Version: 1.2.1
-Author: VladVRO
+Version: 1.3.0
+Author: VladVRO, arhohryakov
 
 Using:
 Add next line to lua startup file (SciTEStartup.lua):
@@ -32,6 +32,7 @@ local RootMenuCommands =
 	"Update All|9190|"..
 	"Commit AllЕ|9191|"..
 	"Show log for All|9192|"
+	"Check for modifications for All|9193|"
 local BranchMenuCommands =
 	"||"..
 	"Update 'trunk'|9187|"..
@@ -46,29 +47,26 @@ local function update_svn_menu()
 	local isSVN = false
 	local svnSign
 	-- test SVN context
-	if shell.fileexists(filedir.."\\.svn") then
-		isSVN = true
-		svnSign = "."
-	elseif shell.fileexists(filedir.."\\_svn") then
-		isSVN = true
-		svnSign = "_"
-	end
+  repeat
+		if shell.fileexists(filedir.."\\.svn") then
+			isSVN = true
+			svnSign = "."
+			break
+		elseif shell.fileexists(filedir.."\\_svn") then
+			isSVN = true
+			svnSign = "_"
+			break
+		end
+		-- move to parent folder
+		filedir = string.match(filedir, "(.*)\\")
+  until (filedir == nil)
+	
 	if isSVN then
 		-- file in SVN context
 		svnroot = filedir
-		local filemenu = FileMenuCommands
+		local filemenu = NewFileMenuCommands.."||"..FileMenuCommands
 		local branchmenu = ""
 		local child = ""
-		-- open SVN data file
-		local entries = io.open(filedir.."\\"..svnSign.."svn\\entries")
-		if entries ~= nil then
-			entries:seek("set")
-			local data = entries:read("*a")
-			entries:close()
-			if not string.find(data, "\n"..props["FileNameExt"].."\n", 1, 1) then
-				filemenu = NewFileMenuCommands
-			end
-		end
 		-- find SVN branch/trunk and root
 		repeat
 			local _,_,parent,name = string.find(svnroot, "(.*)\\([^\\]+)")
