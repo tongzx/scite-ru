10f1738f1f0ee1eacac0931b2c23c0c119681f52
mozers <mozers@localhost>
2015-04-23 14:56:30 +0300

    -chg: RegJump.js ???? ?????????????. ?? ????????? ???? ??? ?? ???.?????? ??? ? ?? ?????? ??????. ?????? ??? ????? ???????????? ??? ? SciTE ??? ? ????????.

--- pack/tools/RegJump.js
+++ pack/tools/RegJump.js
@@ -1,35 +1,54 @@
 /*
 Registry Jump
-Version: 2.0
-Author: mozers™
+Version: 3.0
+Author: mozersв„ў <http://html-applications.bitbucket.org>
 ------------------------------------------------
-Открывает выделенную ветвь в редакторе реестра
-Понимает записи вида:
+Open a new instance of the Registry Editor on the specified key.
+The key can be input to the script in two ways:
+В  1. Through the command line argument
+В  2. Through the clipboard (The key is to be copied into a buffer before running the script)
+
+Recognize entries of the any form:
   HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control
-  HKLM\SYSTEM\CurrentControlSet\Control
+  [HKLM\SYSTEM\CurrentControlSet\Control]
   HKLM\\SYSTEM\\CurrentControlSet\\Control
-Подключение:
-command.name.78.*=Registry Jump
-command.78.*=wscript "$(SciteDefaultHome)\tools\RegJump.js"
-command.input.78.*=$(CurrentSelection)
-command.mode.78.*=subsystem:windows,replaceselection:no,savebefore:no,quiet:yes
-command.shortcut.78.*=Ctrl+Alt+J
 */
 
-var key = WScript.StdIn.ReadAll();
-if (key === "") {
-	WScript.Quit();
-}
 var WshShell = new ActiveXObject("WScript.Shell");
 
-var LastKey = 'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Applets\\Regedit\\Lastkey';
-key = key.replace(/^HKLM\\/,'HKEY_LOCAL_MACHINE\\');
-key = key.replace(/^HKCR\\/,'HKEY_CLASSES_ROOT\\');
-key = key.replace(/^HKCU\\/,'HKEY_CURRENT_USER\\');
-key = key.replace(/\\\\/g,'\\');
-key = WshShell.RegRead(LastKey).match(/^[^\\]+/) + '\\' + key;
+var oArgs = WScript.Arguments;
+var key = oArgs.length ? oArgs(0) : '';
 
-WshShell.RegWrite (LastKey, key,'REG_SZ');
-WshShell.Run('regedit -m', 1, false);
-WScript.Quit();
+if (!key) {
+	try {
+		key = (new ActiveXObject("htmlfile")).parentWindow.clipboardData.getData('Text');
+	} catch(e) {}
+}
 
+if (key) {
+	key = key.replace(/^HKLM\\/,'HKEY_LOCAL_MACHINE\\');
+	key = key.replace(/^HKCU\\/,'HKEY_CURRENT_USER\\');
+	key = key.replace(/^HKCR\\/,'HKEY_CLASSES_ROOT\\');
+	key = key.replace(/^HKU\\/, 'HKEY_USERS\\');
+	key = key.replace(/^HKCC\\/,'HKEY_CURRENT_CONFIG\\');
+	key = key.replace(/\\\\/g,'\\');
+	key = key.replace(/^\s*\[/,'').replace(/\]\s*$/,'');
+}
+
+if (/^HKEY_/i.test(key)) {
+	try {
+		WshShell.RegRead(key + '\\');
+	} catch(e) {
+		WshShell.Popup("Key\n" + key + "\nnot exist!", 3, "Registry Jump", 48);
+	}
+
+	var LastKey = 'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Applets\\Regedit\\Lastkey';
+	try {
+		var computer = WshShell.RegRead(LastKey).replace(/\\.+$/, '') || 'Computer';
+		WshShell.RegWrite (LastKey, computer + '\\' + key, 'REG_SZ');
+	} catch(e){};
+} else {
+	WshShell.Popup("Registry key not defined!\nStart RegEdit default...", 2, "Registry Jump", 48);
+}
+
+WshShell.Run('regedit -m', 1);
