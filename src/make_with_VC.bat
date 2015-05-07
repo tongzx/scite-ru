6fa49f3b743a3a50d13bf8396374c617c058ff3c
mozers <mozers@localhost>
2015-05-07 20:40:30 +0300

    -chg: ?????????? ????????? ????????? ?? ??????? ???????? Dmitrii Evdokimov.

--- src/make_with_VC.bat
+++ src/make_with_VC.bat
@@ -3,15 +3,12 @@ REM for make debug version use:
 REM >make_with_VC.bat DEBUG
 
 setlocal
-set VC7=%ProgramFiles%\Microsoft Visual Studio .NET
-set VC71=%ProgramFiles%\Microsoft Visual Studio .NET 2003
-set VC8=%ProgramFiles%\Microsoft Visual Studio 8
-set VC9=%ProgramFiles%\Microsoft Visual Studio 9.0
-set VC10=%ProgramFiles%\Microsoft Visual Studio 10.0
-set VC12=%ProgramFiles%\Microsoft Visual Studio 12.0
-set Tools=Common7\Tools
-set path=%VC12%\%Tools%;%VC10%\%Tools%;%VC9%\%Tools%;%VC8%\%Tools%;%VC71%\%Tools%;%VC7%\%Tools%;%path%
-call vsvars32.bat
+FOR /f "tokens=2 delims==" %%a IN ('SET ^| FINDSTR /b /i /r /c:"VS[0-9]*COMNTOOLS"') DO SET Tools=%%a
+if "%Tools%"=="" (
+	echo VS COMNTOOLS Environment NOT FOUND!
+	exit /b 1
+)
+call "%Tools%vsvars32.bat"
 
 if "%1"=="DEBUG" set parameter1=DEBUG=1
 
